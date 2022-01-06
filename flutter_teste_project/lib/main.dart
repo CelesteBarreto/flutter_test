752425925f949268c44c8649d70b0cf57ad1ece9
Celeste Silva <silvaccbds@gmail.com>
2022-01-06 15:20:11 -0300

    changes to conflict

--- flutter_teste_project/lib/main.dart
+++ flutter_teste_project/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Column(
               children: [
                 Text("Chbanges into development branch"),
-                Text("Chbanges into development branch 2"),
+                Text("Chbanges into development branch 2 'changesto conflict'"),
                 Text(
                   'You have pushed the button this many times:',
                 ),
